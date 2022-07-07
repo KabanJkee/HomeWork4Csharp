c75eecd93315d57d8bb8f5a158db8feadb0abf92
KabanJkee <KabanJkee@gmail.com>
2022-07-07 16:49:01 +0300

    add homework4

---
+++ ex001/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число.");
+        }
+    }
+
+    return result;
+}
+
+int A = GetNumber("Введите число А:");
+int B = GetNumber("Введите число B:");
+
+Console.WriteLine($"Число {A} в степени {B} = {Math.Pow(A,B)}");
---
+++ ex002/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+
+int GetNumber()
+{
+    int result = 0;
+    Console.WriteLine("Введите число:");
+    while(true)
+    {
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+
+int number = Math.Abs(GetNumber());
+int sum = 0;
+int num = number;
+
+while (number != 0) 
+{
+    sum = sum + number % 10;
+    number /= 10;
+}
+Console.WriteLine($"Сумма цифр в числе {num} равно {sum}");
+
+
